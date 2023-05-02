5841d47d92ac8677d9e3c15615f14ecd531fcd35
rovezuka <rostislav.vetoshkin@gmail.com>
2023-05-02 03:55:16 +0300

    work with arithmetic operators

--- main.go
+++ main.go
@@ -13,4 +13,27 @@ func main() {
 	x, y := 18, 19
 	fmt.Println(x, y)
 	fmt.Println(pi)
+	var a int = 31
+	var b int = 14
+	var result int
+
+	// сложение
+	result = a + b
+	fmt.Println(result) // выведет 45
+
+	// вычитание
+	result = a - b
+	fmt.Println(result) // выведет 17
+
+	// умножение
+	result = a * b
+	fmt.Println(result) // выведет 434
+
+	// деление
+	result = a / b
+	fmt.Println(result) // выведет 2
+
+	// остаток от деления
+	result = a % b
+	fmt.Println(result) // выведет 3
 }
