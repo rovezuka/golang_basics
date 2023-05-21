effa6ab0506ab61f4b309d4d8e5538cdc5cd0041
rovezuka <rostislav.vetoshkin@gmail.com>
2023-05-21 03:04:35 +0300

    arrays

--- main.go
+++ main.go
@@ -7,177 +7,197 @@ func main() {
 	*/
 
 	/*
-						// контстанта и iota
-						const pi = 3.14
-						fmt.Println(pi)
-						const (
-							a = iota // 0
-							b
-							c
-							d // 3, так как iota каждый раз увеличивается на 1
-						)
+							// контстанта и iota
+							const pi = 3.14
+							fmt.Println(pi)
+							const (
+								a = iota // 0
+								b
+								c
+								d // 3, так как iota каждый раз увеличивается на 1
+							)
 
-						// вывод текста через функцию Println из пакета "fmt"
-						fmt.Println("Hello, world!")
+							// вывод текста через функцию Println из пакета "fmt"
+							fmt.Println("Hello, world!")
 
-						// объявление переменных
-						x, y := 18, 19
-						fmt.Println(x, y)
+							// объявление переменных
+							x, y := 18, 19
+							fmt.Println(x, y)
 
-						// арифмитические операции
-						var a int = 31
-						var b int = 14
-						var result int
+							// арифмитические операции
+							var a int = 31
+							var b int = 14
+							var result int
 
-						// сложение
-						result = a + b
-						fmt.Println(result) // выведет 45
+							// сложение
+							result = a + b
+							fmt.Println(result) // выведет 45
 
-						// вычитание
-						result = a - b
-						fmt.Println(result) // выведет 17
+							// вычитание
+							result = a - b
+							fmt.Println(result) // выведет 17
 
-						// умножение
-						result = a * b
-						fmt.Println(result) // выведет 434
+							// умножение
+							result = a * b
+							fmt.Println(result) // выведет 434
 
-						// деление
-						result = a / b
-						fmt.Println(result) // выведет 2
+							// деление
+							result = a / b
+							fmt.Println(result) // выведет 2
 
-						// остаток от деления
-						result = a % b
-						fmt.Println(result) // выведет 3
-						c := "Привет, "
-						d := "Мир!"
-						fmt.Println(c + d) // выведет: Привет, Мир!
+							// остаток от деления
+							result = a % b
+							fmt.Println(result) // выведет 3
+							c := "Привет, "
+							d := "Мир!"
+							fmt.Println(c + d) // выведет: Привет, Мир!
 
-						// операторы присваивания
-						a += b
-						fmt.Println(a) // выведет 45
+							// операторы присваивания
+							a += b
+							fmt.Println(a) // выведет 45
 
-						// инкремент и декремент
-						a++ // аналог выражения a = a + 1
-						fmt.Println(a) // выведет 3
-						a-- // аналог выражения a = a - 1
-						fmt.Println(a) // выведет 1
+							// инкремент и декремент
+							a++ // аналог выражения a = a + 1
+							fmt.Println(a) // выведет 3
+							a-- // аналог выражения a = a - 1
+							fmt.Println(a) // выведет 1
 
-						// операторы сравнения и логические операторы
-						// равно
-						fmt.Println(a == b) // выведет false
+							// операторы сравнения и логические операторы
+							// равно
+							fmt.Println(a == b) // выведет false
 
-						// не равно
-						fmt.Println(a != b) // выведет true
+							// не равно
+							fmt.Println(a != b) // выведет true
 
-						// больше чем
-						fmt.Println(a > b) // выведет true
+							// больше чем
+							fmt.Println(a > b) // выведет true
 
-						// логическое И
-						fmt.Println(a != b && a >= b) // выведет true
+							// логическое И
+							fmt.Println(a != b && a >= b) // выведет true
 
-						// логическое ИЛИ
-						fmt.Println(a == b || a > b) // выведет true
+							// логическое ИЛИ
+							fmt.Println(a == b || a > b) // выведет true
 
-						// логическое НЕ
-						fmt.Println(!(a < b)) // выведет true
+							// логическое НЕ
+							fmt.Println(!(a < b)) // выведет true
 
-						// ввод
-						// вывод суммы чисел
-						var j int
-						var k int
-						fmt.Scan(&j)
-						fmt.Scan(&k)
-						fmt.Println(j + k)
+							// ввод
+							// вывод суммы чисел
+							var j int
+							var k int
+							fmt.Scan(&j)
+							fmt.Scan(&k)
+							fmt.Println(j + k)
 
-						// вывод через Scanln
+							// вывод через Scanln
 
-						var j, k int
-						fmt.Scanln(&j, &k)
-						fmt.Println(j + k)
+							var j, k int
+							fmt.Scanln(&j, &k)
+							fmt.Println(j + k)
 
 
-						// условный оператор if/else
-						var pass int
-						pass = 7055
-						if pass == 7055 {
-							fmt.Println("Пароль верный")
-						}
-						// пароль верный, условие равно true
-						// код будет выволнен и выведет: Пароль верный
+							// условный оператор if/else
+							var pass int
+							pass = 7055
+							if pass == 7055 {
+								fmt.Println("Пароль верный")
+							}
+							// пароль верный, условие равно true
+							// код будет выволнен и выведет: Пароль верный
 
-						pass = 7055
-						if pass == 3142 {
-							fmt.Println("Пароль неверный")
-						}
-						// пароль неверный, условие равно false
-						// код не будет выполнен и ничего не будет выведено
+							pass = 7055
+							if pass == 3142 {
+								fmt.Println("Пароль неверный")
+							}
+							// пароль неверный, условие равно false
+							// код не будет выполнен и ничего не будет выведено
 
-						// else
-						iq := 70
+							// else
+							iq := 70
 
-						if iq > 120 {
-							fmt.Println("Вы гений!")
+							if iq > 120 {
+								fmt.Println("Вы гений!")
+							} else {
+								fmt.Println("Увы, но вы не гений")
+							}
+
+
+						// else if
+						var a, b int
+						fmt.Scan(&a)
+						fmt.Scan(&b)
+						if a > b {
+							fmt.Println("a больше чем b")
+						} else if a == b {
+							fmt.Println("a равно b")
+						} else if c := 100 - 1; c == 100 {
+							fmt.Println("проверка работы условий")
 						} else {
-							fmt.Println("Увы, но вы не гений")
+							fmt.Println("a меньше чем b")
 						}
 
 
-					// else if
-					var a, b int
-					fmt.Scan(&a)
-					fmt.Scan(&b)
-					if a > b {
-						fmt.Println("a больше чем b")
-					} else if a == b {
-						fmt.Println("a равно b")
-					} else if c := 100 - 1; c == 100 {
-						fmt.Println("проверка работы условий")
-					} else {
-						fmt.Println("a меньше чем b")
+					// оператор switch
+					x := 2
+
+					switch x {
+					case 1:
+						fmt.Println("Один")
+					case 2:
+						fmt.Println("Два")
+					case 3:
+						fmt.Println("Три")
+					default:
+						fmt.Println(x)
 					}
 
 
-				// оператор switch
-				x := 2
-
-				switch x {
-				case 1:
-					fmt.Println("Один")
-				case 2:
-					fmt.Println("Два")
-				case 3:
-					fmt.Println("Три")
-				default:
-					fmt.Println(x)
+				// цикл for
+				for i := 0; i <= 10; i++ {
+					fmt.Println(i)
 				}
 
+				sum_even := 0
 
-			// цикл for
-			for i := 0; i <= 10; i++ {
-				fmt.Println(i)
-			}
-
-			sum_even := 0
-
-			for i := 1; i <= 10; i++ {
-				if i%2 != 0 {
-					continue // число нечетное, то пропускаем его
-				} // и переходим к следующей итерации
-				sum_even += i
-			}
-			fmt.Println(sum_even) // выведет: 30
+				for i := 1; i <= 10; i++ {
+					if i%2 != 0 {
+						continue // число нечетное, то пропускаем его
+					} // и переходим к следующей итерации
+					sum_even += i
+				}
+				fmt.Println(sum_even) // выведет: 30
 
 
-		// форматированный вывод
-		var a rune = 'Ы'
-		fmt.Printf("%q", a)
-		// вывод: 'Ы'
-		var a float64 = 100.123456789
-		result := fmt.Sprintf("%.2f", a)
-		fmt.Printf("%q", result) // вывод: "100.12"
-		// result будет типа string
+			// форматированный вывод
+			var a rune = 'Ы'
+			fmt.Printf("%q", a)
+			// вывод: 'Ы'
+			var a float64 = 100.123456789
+			result := fmt.Sprintf("%.2f", a)
+			fmt.Printf("%q", result) // вывод: "100.12"
+			// result будет типа string
 
 
+			// массивы
+			var a [3]int = [3]int{1, 2, 3}
+			b := [3]int{1, 2, 3}
+			c := [...]int{1, 2, 3}
+			d := [3]int{1: 12}
 
+			fmt.Println(a) // [1 2 3]
+			fmt.Println(b) // [1 2 3]
+			fmt.Println(c) // [1 2 3]
+			fmt.Println(d) // [0 12 0]
+
+
+		a := make([]int, 10, 10) // [0 0 0 0 0 0 0 0 0 0]
+		for idx, elem := range a {
+		fmt.Printf("Элемент с индексом %d: %d\n", idx, elem)
+		// Элемент с индексом 0: 1
+		// Элемент с индексом 1: 2
+		// Элемент с индексом 2: 3
+		// Элемент с индексом 3: 4
+		// Элемент с индексом 4: 5
+		}
 	*/
 }
