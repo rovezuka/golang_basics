de4549a03568116e6d0a828148f1085bc45ff0c1
rovezuka <rostislav.vetoshkin@gmail.com>
2023-06-05 23:38:44 +0300

    update schedule

--- go_practice/Go-Modules-Packages/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/rovezuka/golang_basics/tree/main/go_practice/Go-Modules-Packages
-
-go 1.20
-
-require github.com/zhashkevych/scheduler v1.0.0 // indirect
--- go_practice/Go-Modules-Packages/main.go
+++ go_practice/Go-Modules-Packages/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/zhashkevych/scheduler"
+)
 
 func main() {
-	fmt.Print("Hello")
+	s := scheduler.NewScheduler()
+
+	s.Add(context.Background(), func(ctx context.Context) {
+		fmt.Printf("Текущее время: %s\n", time.Now())
+	}, time.Second*1)
+
+	time.Sleep(time.Minute)
 }
